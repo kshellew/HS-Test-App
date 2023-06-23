eba8380bd3d4dfdb4204bb94470cc3bc19309d50
K'Shelle Waller <kshellew@gmail.com>
2023-06-23 20:02:42 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import HelpScout, { NOTIFICATION_TYPES } from "@helpscout/javascript-sdk";
 import { Button, DefaultStyle, Heading } from "@helpscout/ui-kit";
 import { useEffect, useState,} from "react";
-import song from "./static/a.mp3";
+
 
 function App() {
   let audio = new Audio('./static/a.mp3')
